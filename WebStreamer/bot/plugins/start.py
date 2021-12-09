bd9c6942b94429c0065f4225389c8b64a0613449
github2tg <92420369+github2tg@users.noreply.github.com>
2021-12-09 19:43:02 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -10,21 +10,13 @@ async def start(_, m: Message):
     await m.reply(f'ℍ𝕚 {m.from_user.mention(style="md")}, 𝕊𝕖𝕟𝕕 𝕞𝕖 𝕒 𝕗𝕚𝕝𝕖 𝕥𝕠 𝕘𝕖𝕥 𝕒𝕟 𝕚𝕟𝕤𝕥𝕒𝕟𝕥 𝕤𝕥𝕣𝕖𝕒𝕞 𝕝𝕚𝕟𝕜.\n\nപിന്നെ ഒരുകാര്യം ബോട്ട് start കൊടുത്തിട്ട് reply വരുന്നില്ല എങ്കിൽ താഴെ പറയുന്നലിങ്കിൽ ഒന്ന് ക്ലിക്ക് ചെയ്ത് open ചെയ്ത് തിരിച്ച് വരുക അപ്പൊ ശെരിയാകും\n\n┈┈┈••✿ 👇👇🅻︎🅸︎🅽︎🅺︎👇👇 ✿••┈┈┈.',
                   reply_markup=InlineKeyboardMarkup(
                       [[
-                            InlineKeyboardButton(
-                        "🍬 Project channel", url="https://telegram.me/MHND_BOT_UPDATE_CHANNEL"
-                    ),
-                    InlineKeyboardButton("🍭 Support group", url="https://telegram.me/MHND_BOT_UPDATE_GROUP"),
+                            InlineKeyboardButton('🅻︎🅸︎🅽︎🅺︎', url='https://mhnd-file-streem.herokuapp.com/'),
+                    InlineKeyboardButton('𝕁𝕆𝕀ℕ 𝕋ℍ𝕀𝕊 ℂℍ𝔸ℕℕ𝔼𝕃', url='https://telegram.me/MHND_BOT_UPDATE_CHANNEL')
                 ],
-                [InlineKeyboardButton("👩‍💻 Master", url="https://telegram.me/MHND_KDR")],
+                [
+                    InlineKeyboardButton('🌹 Source code 🌹', url='https://github.com/github2tg/FILE_ANIMATING'),
+                    InlineKeyboardButton('👩‍💻 Master', url='https://telegram.me/MHND_KDR')
+                ]
             ]
-        ),
-    )                         
-_________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________________
-                            InlineKeyboardButton(
-                                  f'🅻︎🅸︎🅽︎🅺︎',
-                                  url='https://mhnd-file-streem.herokuapp.com/')
-                            InlineKeyboardButton(
-                                  f'𝕁𝕆𝕀ℕ 𝕋ℍ𝕀𝕊 ℂℍ𝔸ℕℕ𝔼𝕃',
-                                  url='https://telegram.me/MHND_BOT_UPDATE_CHANNEL')
-                        ]]
-                  ))
+        )
+    
