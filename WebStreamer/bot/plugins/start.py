a170cf858a12fa217b88ac4e64f83d21a54e3756
github2tg <92420369+github2tg@users.noreply.github.com>
2021-12-09 19:51:20 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -9,14 +9,15 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 async def start(_, m: Message):
     await m.reply(f'ℍ𝕚 {m.from_user.mention(style="md")}, 𝕊𝕖𝕟𝕕 𝕞𝕖 𝕒 𝕗𝕚𝕝𝕖 𝕥𝕠 𝕘𝕖𝕥 𝕒𝕟 𝕚𝕟𝕤𝕥𝕒𝕟𝕥 𝕤𝕥𝕣𝕖𝕒𝕞 𝕝𝕚𝕟𝕜.\n\nപിന്നെ ഒരുകാര്യം ബോട്ട് start കൊടുത്തിട്ട് reply വരുന്നില്ല എങ്കിൽ താഴെ പറയുന്നലിങ്കിൽ ഒന്ന് ക്ലിക്ക് ചെയ്ത് open ചെയ്ത് തിരിച്ച് വരുക അപ്പൊ ശെരിയാകും\n\n┈┈┈••✿ 👇👇🅻︎🅸︎🅽︎🅺︎👇👇 ✿••┈┈┈.',
                   reply_markup=InlineKeyboardMarkup(
-                      [[
-                            InlineKeyboardButton('🅻︎🅸︎🅽︎🅺︎', url='https://mhnd-file-streem.herokuapp.com/'),
+            [
+                [
+                    InlineKeyboardButton('🅻︎🅸︎🅽︎🅺︎', url='https://mhnd-file-streem.herokuapp.com/'),
                     InlineKeyboardButton('𝕁𝕆𝕀ℕ 𝕋ℍ𝕀𝕊 ℂℍ𝔸ℕℕ𝔼𝕃', url='https://telegram.me/MHND_BOT_UPDATE_CHANNEL')
                 ],
                 [
-                    InlineKeyboardButton('🌹 Source code 🌹', url='https://github.com/github2tg/FILE_ANIMATING'),
+                    InlineKeyboardButton('🌹 Source code 🌹', url='https://telegram.me/MHND_BOT_UPDATE_CHANNEL/145'),
                     InlineKeyboardButton('👩‍💻 Master', url='https://telegram.me/MHND_KDR')
                 ]
             ]
         )
-    
+    )
