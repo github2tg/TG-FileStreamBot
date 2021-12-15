8dfab76801b91ba42e8468502cf79427c56f844b
github2tg <92420369+github2tg@users.noreply.github.com>
2021-12-15 19:07:53 +0530

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -7,16 +7,16 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 
 @StreamBot.on_message(filters.command(['start', 'help']))
 async def start(_, m: Message):
-    await m.reply(f'ℍ𝕚 {m.from_user.mention(style="md")}, 𝕊𝕖𝕟𝕕 𝕞𝕖 𝕒 𝕗𝕚𝕝𝕖 𝕥𝕠 𝕘𝕖𝕥 𝕒𝕟 𝕚𝕟𝕤𝕥𝕒𝕟𝕥 𝕤𝕥𝕣𝕖𝕒𝕞 𝕝𝕚𝕟𝕜.\n\nപിന്നെ ഒരുകാര്യം ബോട്ട് start കൊടുത്തിട്ട് reply വരുന്നില്ലേ....\t\tതാഴെ കാണുന്ന 🅻︎🅸︎🅽︎🅺︎ എന്ന Box ൽ ക്ലിക്ക് ചെയ്യൂ\n\n🚨 Porn Contents will be gives you PERMANENT BAN 🚨\n\n🍃 Bᴏᴛ Made Bʏ :@MHND_KDR\n\n┈┈┈••✿ 👇👇🅻︎🅸︎🅽︎🅺︎👇👇 ✿••┈┈┈',
+    await m.reply(f'ℍ𝕚 {m.from_user.mention(style="md")}, 𝕊𝕖𝕟𝕕 𝕞𝕖 𝕒 𝕗𝕚𝕝𝕖 𝕥𝕠 𝕘𝕖𝕥 𝕒𝕟 𝕚𝕟𝕤𝕥𝕒𝕟𝕥 𝕤𝕥𝕣𝕖𝕒𝕞 𝕝𝕚𝕟𝕜.\n\nപിന്നെ ഒരുകാര്യം ബോട്ട് start കൊടുത്തിട്ട് reply വരുന്നില്ലേ....\t\tതാഴെ കാണുന്ന 🅻︎🅸︎🅽︎🅺︎ എന്ന Box ൽ ക്ലിക്ക് ചെയ്യൂ\n\n🚨 Porn Contents will be gives you PERMANENT BAN 🚨\n\n🍃 Bᴏᴛ Made Bʏ :@MD_BOTZ_OWNER\n\n┈┈┈••✿ 👇👇🅻︎🅸︎🅽︎🅺︎👇👇 ✿••┈┈┈',
                   reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton('🅻︎🅸︎🅽︎🅺︎', url='tg://resolve?domain=MHND_BOT_UPDATE_CHANNEL&post=150'),
-                    InlineKeyboardButton('𝕁𝕆𝕀ℕ 𝕋ℍ𝕀𝕊 ℂℍ𝔸ℕℕ𝔼𝕃', url='https://telegram.me/MHND_BOT_UPDATE_CHANNEL')
+                    InlineKeyboardButton('🅻︎🅸︎🅽︎🅺︎', url='tg://resolve?domain=MD_BOTZ&post=13'),
+                    InlineKeyboardButton('𝕁𝕆𝕀ℕ 𝕋ℍ𝕀𝕊 ℂℍ𝔸ℕℕ𝔼𝕃', url='https://telegram.me/MD_BOTZ')
                 ],
                 [
                     InlineKeyboardButton('🌹 Source code 🌹', url='https://github.com/EverythingSuckz/TG-FileStreamBot'),
-                    InlineKeyboardButton('👩‍💻 Master', url='https://telegram.me/MHND_KDR')
+                    InlineKeyboardButton('👩‍💻 Master', url='https://telegram.me/MD_BOTZ_OWNER')
                 ]
             ]
         )
