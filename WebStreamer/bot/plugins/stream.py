b79b9b70a133a9cfcc6209c7a05733bc23b82f4f
github2tg <92420369+github2tg@users.noreply.github.com>
2021-12-17 21:07:45 +0530

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -16,17 +16,18 @@ def detect_type(m: Message):
     elif m.audio:
         return m.audio
     else:
-        return
+        return web.Response(status=429)
+        log.info(f"Serving file in {message.id} (chat {message.chat_id}) to {ip}")
+        body = transfer.download(message.media, file_size=size, offset=offset, limit=limit)
+    else:
+        body = None
+    return web.Response(status=206 if offset else 200,
+                        body=body,
+                        headers={
+                            "Content-Type": message.file.mime_type,
+                            "Content-Range": f"bytes {offset}-{size}/{size}",
+                            "Content-Length": str(limit - offset),
+                            "Accept-Ranges": "bytes",
+                        })
     
 
-@StreamBot.on_message(filters.private & (filters.document | filters.video | filters.audio), group=4)
-async def media_receive_handler(_, m: Message):
-    file = detect_type(m)
-    file_name = ''
-    if file:
-        file_name = file.file_name
-    log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
-    stream_link = Var.URL + str(log_msg.message_id) + '/' +quote_plus(file_name) if file_name else ''
-    await m.send_text(
-        text="`{}`".format(stream_link),
-    )
